e14a8bd387e7b37810e36a5367c1487ca7834662
hzhjq <hzhuangjianqiang@corp.netease.com>
2021-12-06 19:19:18 +0800

    feat: add 404 401 1919

--- src/components/lcap-h5-401.vue/index2.vue
+++ src/components/lcap-h5-401.vue/index2.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="h5template-401" vusion-slot-name="default"></div>
+  <div class="h5template-401" vusion-slot-name="default">
+    <div class="m401-image-wrap">
+      <van-image
+        class="m401-image"
+        src="//static-vusion.nos-eastchina1.126.net/h5-template/401_h5.png"
+      >
+      </van-image>
+      <div class="m401-image-text">你没有访问该页面的权限</div>
+      <div class="m401-image-btn" @click="toLogin">重新登录</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,9 +18,55 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    toLogin() {
+      window.location.href = "/login";
+    },
+  },
 };
 </script>
 
 <style lang="less">
+// .h5template-401 {
+//   height: 100vh;
+//   background-color: #fff;
+
+//   .m401-image-wrap {
+//     margin-top: 37.33333vw;
+
+//     .m401-image {
+//       display: block;
+//       width: 42.66667vw;
+//       margin-left: auto;
+//       margin-right: auto;
+//     }
+
+//     .m401-image-text {
+//       margin-top: 4.26667vw;
+//       text-align: center;
+//       font-family: PingFang SC;
+//       font-style: normal;
+//       font-weight: normal;
+//       font-size: 14px;
+//       color: #999;
+//     }
+
+//     .m401-image-btn {
+//       width: 25.6vw;
+//       height: 8.53333vw;
+//       line-height: 8.53333vw;
+//       background: #337eff;
+//       border-radius: 4px;
+//       font-family: PingFang SC;
+//       font-style: normal;
+//       font-weight: normal;
+//       font-size: 14px;
+//       color: #fff;
+//       text-align: center;
+//       margin-left: auto;
+//       margin-right: auto;
+//       margin-top: 40px;
+//     }
+//   }
+// }
 </style>
--- src/components/lcap-h5-404.vue/index2.vue
+++ src/components/lcap-h5-404.vue/index2.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="h5template-404" vusion-slot-name="default"></div>
+  <div class="h5template-404" vusion-slot-name="default">
+    <div class="m404-image-wrap">
+      <van-image
+        class="m404-image"
+        src="//static-vusion.nos-eastchina1.126.net/h5-template/404_h5.png"
+      >
+      </van-image>
+      <div class="m404-image-text">抱歉，你访问的页面不存在</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +22,29 @@ export default {
 </script>
 
 <style lang="less">
+// .h5template-404 {
+//   height: 100vh;
+//   background-color: #fff;
+
+//   .m404-image-wrap {
+//     margin-top: 37.33333vw;
+
+//     .m404-image {
+//       display: block;
+//       width: 42.66667vw;
+//       margin-left: auto;
+//       margin-right: auto;
+//     }
+
+//     .m404-image-text {
+//       margin-top: 4.26667vw;
+//       text-align: center;
+//       font-family: PingFang SC;
+//       font-style: normal;
+//       font-weight: normal;
+//       font-size: 14px;
+//       color: #999;
+//     }
+//   }
+// }
 </style>
